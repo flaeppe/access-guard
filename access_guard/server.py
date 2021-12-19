61d29add85939b10bfb6a2c50f0be7adf62ae08f
Petter Friberg <petter_friberg@hotmail.com>
2021-12-19 21:28:04 +0100

    Log access-guard configuration on startup

--- access_guard/server.py
+++ access_guard/server.py
@@ -9,10 +9,31 @@ from starlette.routing import Mount, Route
 from starlette.staticfiles import StaticFiles
 
 from . import healthcheck, settings
-from .log import LOGGING_CONFIG
+from .log import LOGGING_CONFIG, logger
 from .routes.auth import auth
 from .routes.send import send
 from .routes.verify import verify
+
+
+async def log_settings() -> None:
+    logger.info(
+        "loaded_config",
+        email_patterns=[compiled.pattern for compiled in settings.EMAIL_PATTERNS],
+        auth_host=str(settings.AUTH_HOST),
+        cookie_domain=settings.COOKIE_DOMAIN,
+        cookie_secure=settings.COOKIE_SECURE,
+        email_host=settings.EMAIL_HOST,
+        email_port=settings.EMAIL_PORT,
+        auth_cookie_name=settings.AUTH_COOKIE_NAME,
+        verified_cookie_name=settings.VERIFIED_COOKIE_NAME,
+        auth_cookie_max_age=settings.AUTH_COOKIE_MAX_AGE,
+        auth_signature_max_age=settings.AUTH_SIGNATURE_MAX_AGE,
+        verify_signature_max_age=settings.VERIFY_SIGNATURE_MAX_AGE,
+        host=settings.HOST,
+        port=settings.PORT,
+        debug=settings.DEBUG,
+    )
+
 
 routes = [
     Mount(
@@ -41,7 +62,7 @@ app = Starlette(
     routes=routes,
     middleware=middleware,
     debug=settings.DEBUG,
-    on_startup=[healthcheck.check_smtp],
+    on_startup=[log_settings, healthcheck.check_smtp],
 )
 
 
